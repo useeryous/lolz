cae838853eed881a002e89f90bbe3c957281f73d
lemon <103284423+useeryous@users.noreply.github.com>
2025-05-02 20:13:36 +0000

    deleted:    downloads/install.bat 	deleted:    downloads/install.sh 	modified:   main.py 	new file:   style.tcss

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Button, Label, Static
-from textual.containers import VerticalScroll
-from textual.reactive import reactive
-from textual.events import Click
+from textual.widgets import Tree, Tabs, Tab, TextArea
 
-class MyApp(App):
-    C
+class MainApp(App):
+    CSS_PATH = "style.tcss"
+
+    def composs(self) -> ComposeResult:
+
+        #file tree
+        tree: Tree[str] = tree("dune")
+        tree.root.expand()
+
+        #tabs
+        yield Tabs(
+            Tab("help.txt", id="tab1")
+        )
+
+        #editor
+        yield TextArea.code.editor(language="python")
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
